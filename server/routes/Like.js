5c7d7dc80248bd57795a63662bb4c6b2e0f9dd84
Marczeli <aleczas1@onet.pl>
2020-12-18 15:00:07 +0100

    Server added like route. Possible changes

---
+++ server/routes/Like.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const like = require('../controllers/like');
+const auth = require('../middleware/auth');
+
+router.post('/getLikes', auth, like.getLikes);
+router.post('/getDislikes', auth, like.getDislikes);
+router.post('/upLike', auth, like.upLike);
+router.post('/unLike', auth, like.unLike);
+router.post('/upDisLike', auth, like.upDisLike);
+router.post('/unDisLike', auth, like.unDisLike);
+
+module.exports = router;
+
